5cac59660fe664e3cae1a5bf767baf55af785845
Agnibhu Mandal <82824311+agnibhu-1902@users.noreply.github.com>
2022-09-09 20:31:17 +0530

    Add files via upload

---
+++ C++/multiple_inheritance/professor.cpp
@@ -0,0 +1,18 @@
+/*************************************************************** * The implementation file for the Professor class * ***************************************************************/
+#include "professor.h"
+// Constructor
+Professor :: Professor (string nm, double sal)
+: Person (nm), salary (sal)
+{
+}
+// Destructor
+Professor :: ~Professor ()
+{
+}
+// Print member function
+void Professor :: print ()
+{
+    cout << "Professor " << endl;
+    cout << "Name: " << name << " ";
+    cout << "Salary: " << salary << endl << endl;
+}
