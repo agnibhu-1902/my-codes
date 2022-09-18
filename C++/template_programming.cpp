2bdd8a64e02e2da30615c24e9fbbcaad562b2f55
Agnibhu Mandal <82824311+agnibhu-1902@users.noreply.github.com>
2022-09-19 01:46:08 +0530

    Add files via upload

--- C++/template_programming.cpp
+++ C++/template_programming.cpp
@@ -15,5 +15,6 @@ int main(void)
     cout << "Smaller of a and B: " << smaller('a', 'B') << endl;
     cout << "Smaller of 12 and 15: " << smaller(12, 15) << endl;
     cout << "Smaller of 44.2 and 33.1: " << smaller(44.2, 33.1) << endl;
+    cout << "Smaller of 44.0 and 33.1: " << smaller <double>(44, 33.1) << endl;
     return 0;
 }
