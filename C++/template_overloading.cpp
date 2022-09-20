22f88d12d03d09256942838822b6d07231f90de0
Agnibhu Mandal <82824311+agnibhu-1902@users.noreply.github.com>
2022-09-20 10:26:32 +0530

    Add files via upload

---
+++ C++/template_overloading.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+T smallest(const T& first, const T& second)
+{
+    if (first < second)
+        return first;
+    return second;
+}
+
+template <typename T>
+T smallest(const T& first, const T& second, const T& third)
+{
+    return smallest(smallest(first, second), third);
+}
+
+int main(void)
+{
+    // Calling the overloaded version with three integers
+    cout << "Smallest of 17, 12, and 27 is ";
+    cout << smallest (17, 12, 27) << endl;
+    // Calling the overloaded version with three doubles
+    cout << "Smallest of 8.5, 4.1, and 19.75 is ";
+    cout << smallest (8.5, 4.1, 19.75) << endl;
+    return 0;
+}
